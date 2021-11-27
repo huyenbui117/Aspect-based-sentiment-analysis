a29f76597f14362502860af9eb2e5b12db599671
huyenbui117 <62584585+huyenbui117@users.noreply.github.com>
2021-11-27 22:10:54 +0700

    Fix CORS

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import fastapi
-from fastapi import File, UploadFile
-from fastapi.responses import FileResponse
-from main import run
 import pandas as pd
+from fastapi import File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+
+from main import run
+
 app = fastapi.FastAPI()
+
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post(path="/demo", response_class=FileResponse)
